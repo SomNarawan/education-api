a201f815492c8a8554b86014993106b6866e8b2a
Narawam-Dev <nrw.narawan@gmail.com>
2026-09-20 00:05:40 +0700

    feat: remove CurriculumPlanController and its route from the API

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\AdmissionChannelController;
 use App\Http\Controllers\Api\CurriculumCategoryController;
 use App\Http\Controllers\Api\CurriculumPersonnelController;
-use App\Http\Controllers\Api\CurriculumPlanController;
 use App\Http\Controllers\Api\DataImportController;
 use App\Http\Controllers\Api\HighSchoolController;
 use App\Http\Controllers\Api\ImportTypeController;
@@ -97,7 +96,6 @@ Route::prefix('import-types')->group(function (): void {
     Route::match(['put', 'patch'], '/{id}', [ImportTypeController::class, 'update'])->whereNumber('id');
     Route::patch('/{id}/status', [ImportTypeController::class, 'updateStatus'])->whereNumber('id');
 });
-Route::get('/study-plans', [CurriculumPlanController::class, 'index']);
 Route::get('/curriculum-personnel', [CurriculumPersonnelController::class, 'index']);
 Route::get('/curriculum-categories', [CurriculumCategoryController::class, 'index']);
 Route::get('/imports', [DataImportController::class, 'index']);
